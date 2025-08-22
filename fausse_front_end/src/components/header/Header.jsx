f317a44a66f52955f43056f9709025a49e61d480
Reinis Janovskis <reinis.janovskis@hotmail.com>
2025-08-22 10:36:42 +0100

    Add style to header

--- fausse_front_end/src/components/header/Header.jsx
+++ fausse_front_end/src/components/header/Header.jsx
@@ -6,24 +6,47 @@ import "./Header.css";
 function Header() {
   return (
     <header>
-      <nav className="navbar bg-body-tertiary">
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/menu">Menu</NavLink>
-          </li>
-          <li>
-            <NavLink to="/reservation">Reservation</NavLink>
-          </li>
-          <li>
-            <NavLink to="/gallery">Gallery</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About Us</NavLink>
-          </li>
-        </ul>
+      {/* <nav> is built using bootstrap navbar scaffoldings. All the included classes are meant to be make it responsive */}
+      <nav className="navbar navbar-expand-md">
+        <div className="container-fluid">
+          <Link to="/" classNmae="navbar-brand">
+            <img src="./src/assets/cafe_fausse.jpg" alt="Café Fausse logo" />
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <NavLink to="/" className="nav-link">
+                Home
+              </NavLink>
+
+              <NavLink to="/menu" className="nav-link">
+                Menu
+              </NavLink>
+
+              <NavLink to="/reservation" className="nav-link">
+                Reservation
+              </NavLink>
+
+              <NavLink to="/gallery" className="nav-link">
+                Gallery
+              </NavLink>
+
+              <NavLink to="/about" className="nav-link">
+                About Us
+              </NavLink>
+            </div>
+          </div>
+        </div>
       </nav>
     </header>
   );
